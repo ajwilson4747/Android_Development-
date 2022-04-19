b43f0c0ad63d70bc67f06db49fc5efcadbf4de2b
ajwilson4747 <83085316+ajwilson4747@users.noreply.github.com>
2022-04-18 21:09:45 -0600

    Fibonacci Flower Problem
    Fibonacci flower by adding two types of petals. The app algorithm must position, rotate, and scale each petal before it is added to the screen. The user should be allowed to erase the petals to rebuild a new flower.

--- MainActivity.java
+++ MainActivity.java
@@ -1,115 +1,112 @@
-package com.example.bmicalculator;
+package com.example.fibonacciflower;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
-import android.view.View.OnClickListener;
-import android.widget.RadioGroup;
-import android.widget.RadioButton;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-        //instantiate the variables
+    private ArrayList <ImageView> allPetals;
+    private LayoutInflater layoutInflater;
 
-        private BMI myBMI;
-        private TextView quanHeight;
-        private TextView quanWeight;
-        private TextView totalSummary;
-        private TextView whParameter1;
-        private TextView whParameter2;
-        private RadioGroup converterType;
+    private Button pinkBtn;
+    private Button goldBtn;
+    private Button clearBtn;
+    private RelativeLayout relativeLayout;
 
-        private RadioButton selection1;
-        private RadioButton selection2;
-
-        private Button calculate;
-
-        private boolean usedMetrics=false;
-        private boolean usedStandard=false;
+    Flower myFlower;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myBMI = new BMI();
+        myFlower = new Flower();
+        allPetals = new ArrayList <ImageView> ();
 
-        quanHeight= findViewById(R.id.textView5);
-        quanWeight= findViewById(R.id.textView6);
+        //Initialize generation of the Fibonacci artwork
+        initialize();
 
-        totalSummary = findViewById(R.id.textView7);
+        //create a layout interface to add petals to relative layout
+        layoutInflater = (LayoutInflater) getSystemService (Context.LAYOUT_INFLATER_SERVICE);
 
-        converterType = (RadioGroup) findViewById(R.id.radioGroup);
+        relativeLayout = (RelativeLayout) findViewById (R.id.relativeLayout1);
+        pinkBtn = (Button) findViewById (R.id.button);
+        goldBtn = (Button) findViewById (R.id.button2);
+        clearBtn = (Button) findViewById (R.id.button3);
 
-        selection2 = (RadioButton) findViewById(R.id.radioButton2);
+        pinkBtn.setOnClickListener(addPetal);
+        goldBtn.setOnClickListener(addPetal);
+        clearBtn.setOnClickListener(clearPetals);
 
-        whParameter1 = findViewById(R.id.textView8);
-        whParameter2 = findViewById(R.id.textView9);
+        //set up the center coordinate
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        myFlower.set_xCenter(metrics.widthPixels / 2);
+        myFlower.set_yCenter(metrics.heightPixels/2);
+    }
 
-        calculate = (Button) findViewById(R.id.button6);
+    private void initialize(){
+        //task 1: initialize all the settings for the first petal
+        myFlower.setRotate(0);
+        myFlower.setScaleX((float) .3);
+        myFlower.setScaleY((float) .3);
+        myFlower.setDegenerate((float) 1.001);
+        myFlower.initializeAngle();
+    }
+    //On Click button handlers for adding a flower to the screen
+    private View.OnClickListener addPetal = new View.OnClickListener() {
+        public void onClick (View view){
+            //task 1: instantiate a view to store a petal graphic
+            ImageView petal;
 
+            //Inflate the correct petal
+            String buttonText = ( (Button) view).getText().toString();
+            if (buttonText.equals("Add Pink"))
+                petal = (ImageView) layoutInflater.inflate(R.layout.petal_pink, null);
+            else
+                petal = (ImageView) layoutInflater.inflate(R.layout.petal_gold, null);
+            //task 2: set up the visual properties of the petal
+            petal.setX(myFlower.get_xCenter());
+            petal.setY(myFlower.get_yCenter());
+            petal.setPivotY(0);
+            petal.setPivotX(100);
+            petal.setScaleX(myFlower.getScaleX());
+            petal.setScaleY(myFlower.getScaleY());
+            petal.setRotation(myFlower.getRotate());
 
+            //task 3: place the inflated imageview in the main layout
+            relativeLayout.addView(petal);
 
-        converterType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton rb = (RadioButton) findViewById(checkedId);
-                switch (checkedId){
-                    case R.id.radioButton:
-                        whParameter1.setText("Centimeters");
-                        whParameter2.setText("Kilograms");
-                        usedMetrics = true;
-                        break;
-                    case R.id.radioButton2:
-                        whParameter1.setText("Inches");
-                        whParameter2.setText("Pounds");
-                        usedStandard=true;
-                        break;
-                }
-            }
-        });
+            //Task 4: add the imageview of the petal to the arraylist
+            allPetals.add(petal);
 
-        calculate.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (usedMetrics ==true){
-                    double yourBMIResults = myBMI.computeMetric();
-                    totalSummary.setText("Your BMI Weight is:" + " "+ Double.toString(yourBMIResults));
-                }
-                else{
-                    double yourBMIResults = myBMI.computeStandard();
-                    totalSummary.setText("Your BMI Weight is:" + " " + Double.toString(yourBMIResults));
-                }
-            }
+            //Task 5: update the angle and scale
+            // for the next petal to be added
+            myFlower.updatePetalValues();
         }
-        );
+    };
 
+    //on click button handler to clear all petals on the screen
+    private View.OnClickListener clearPetals = new View.OnClickListener(){
+        public void onClick(View view){
+            //Task 1: Remove all the petal image view from the layout
+            for (int i =0; i<allPetals.size(); i++){
+                ImageView petal = allPetals.get(i);
+                relativeLayout.removeView(petal);
+            }
 
-    }
-
-
-    public void incrementHeight(View view){
-        myBMI.incrementHeight();
-        quanHeight.setText(myBMI.getQuantityHeight());
-    }
-
-    public void decrementHeight(View view){
-        myBMI.decrementHeight();
-        quanHeight.setText(myBMI.getQuantityHeight());
-    }
-
-    public void incrementWeight(View view){
-        myBMI.incrementWeight();;
-        quanWeight.setText(myBMI.getQuantityWeight());
-    }
-
-    public void decrementWeight(View view){
-        myBMI.decrementWeight();
-        quanWeight.setText(myBMI.getQuantityWeight());
-    }
+            //Task 2: clear the arraylist and reset all variables
+            allPetals.clear();
+            initialize();
+        }
+    };
 }
